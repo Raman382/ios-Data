db35191b8dff75c9de6f4e6f78663328ac317b2f
Raman382 <57934402+Raman382@users.noreply.github.com>
2019-11-19 15:36:09 +0530

    Add files via upload

---
+++ DatePickerdate10/DatePickerdate10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DatePickerdate10
+//
+//  Created by trainee on 10/10/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    let dict = NSMutableDictionary()
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+       
+    }
+
+}
+
+
