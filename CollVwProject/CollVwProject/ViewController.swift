f04446f384cdce4ee5088f7c36f3f22bcf91c2ae
Raman382 <57934402+Raman382@users.noreply.github.com>
2019-11-19 11:45:48 +0530

    Add files via upload

---
+++ CollVwProject/CollVwProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollVwProject
+//
+//  Created by trainee on 19/11/19.
+//  Copyright © 2019 trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
