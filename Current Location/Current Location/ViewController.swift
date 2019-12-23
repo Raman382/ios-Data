6f5669f2610d1e4edcaa73084efbc726e8d050f4
Raman382 <57934402+Raman382@users.noreply.github.com>
2019-12-23 13:29:21 +0530

    Add files via upload

---
+++ Current Location/Current Location/ViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    let locale = Locale.current
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager:CLLocationManager = CLLocationManager()
+    //let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        
+
+
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+
+        mapView.showsUserLocation = true
+
+        CLLocationManager.locationServicesEnabled()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        
+    }
+    func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            completion(placemarks?.first?.locality,
+                       placemarks?.first?.country,
+                       error)
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocation = manager.location else { return }
+        fetchCityAndCountry(from: location) { city, country, error in
+            guard let city = city, let country = country, error == nil else { return }
+            print(city + ", " + country)
+        }
+        locationManager.stopUpdatingLocation()
+    }
+}
+
